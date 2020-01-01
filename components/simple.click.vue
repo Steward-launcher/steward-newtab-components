e48581192c0c15d7c67ae7be9c30a65eb4ee5f69
yangjianchuan <yangjianchuan@xatom.io>
2020-01-01 18:00:33 +0800

    add: simple click

---
+++ components/simple.click.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="clock">
+        <div class="time">{{ time }}</div>
+        <div class="date">{{ date }}</div>
+    </div>    
+</template>
+<script>
+const dayjs = window.steward.dayjs
+
+module.exports = {
+    name: 'clock',
+    data() {
+        return {
+            time: dayjs().format('HH:mm'),
+            date: dayjs().format('YYYY-MM-DD')
+        };
+    },
+
+    created() {
+        setInterval(() => {
+            this.count();
+        }, 1000);
+    },
+
+    methods: {
+        count() {
+            this.time = dayjs().format('HH:mm');
+            this.date = dayjs().format('YYYY-MM-DD');
+        }
+    }
+} 
+</script>
+<style>
+    .clock {
+        position: relative;
+    }
+
+    .clock:hover .date {
+      opacity: 1;
+    }
+
+    .clock .time {
+        font-size: 80px;
+        color: #fff;
+        font-weight: 100;
+    }
+
+    .clock .date {
+        position: absolute;
+        bottom: -10px;
+        right: 0.4em;
+        text-align: right;
+        font-size: 20px;
+        color: #fff;
+        font-weight: 100;
+        opacity: 0;
+        transition: all .2s;
+    }
+</style>
+
